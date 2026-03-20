9a1d98c4f5bd46858bed5f3bf3708704ea37333c
ango1710 <ango2506@vk.com>
2026-03-20 22:16:44 +0300

    Add files via upload

---
+++ first.py
@@ -0,0 +1,9 @@
+name = input("Привет,как тебя зовут?")
+print("Приятно познокомиться "+ name)
+age = int(input("Сколько лет? "))
+if age > 14:
+    print("ты большой")
+elif age == 14:
+    print("тож 14?")
+else:
+     print("малой")
